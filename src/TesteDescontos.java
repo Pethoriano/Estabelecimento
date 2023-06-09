1b6f6800378b25cdbdbffeff88b0c9f80c810305
João Pedro <jjppgs@gmail.com>
2023-06-09 18:35:32 -0300

    Implementação - Strategy e chain os responsibility

---
+++ src/TesteDescontos.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+
+import desconto.CalculadoraDeDescontos;
+import orcamento.Orcamento;
+
+public class TesteDescontos {
+    public static void main(String[] args) {
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
+        Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+
+        CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+        System.out.println(calculadora.calcular(primeiro));
+        System.out.println(calculadora.calcular(segundo));
+
+    }
+}
