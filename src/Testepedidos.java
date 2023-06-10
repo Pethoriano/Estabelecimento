749b204ec91d5ebf0f6315b12c3f5f3cd5ee1709
João Pedro <jjppgs@gmail.com>
2023-06-09 23:52:30 -0300

    Implementando - Observer

--- src/Testepedidos.java
+++ src/Testepedidos.java
@@ -1,18 +1,22 @@
 import java.math.BigDecimal;
-
+import java.util.Arrays;
 import pedido.GeraPedido;
 import pedido.GeraPedidoHandler;
+import pedido.acao.EnviarEmail;
+import pedido.acao.SalvarNoBD;
 
 public class TestePedidos {
-    
-public static void main(String[] args) {
-    String cliente = "Julliane";
-    BigDecimal valorOrcamento = new BigDecimal("1000");
-    int quantidadeDeItens = 10;
 
-    GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeDeItens);
-    GeraPedidoHandler handler = new GeraPedidoHandler();
-    handler.execute(gerador);
-}
+    public static void main(String[] args) {
+        String cliente = "Julliane";
+        BigDecimal valorOrcamento = new BigDecimal("1000");
+        int quantidadeDeItens = 10;
+
+        GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeDeItens);
+        GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(
+                new SalvarNoBD(),
+                new EnviarEmail()));
+        handler.execute(gerador);
+    }
 
 }
