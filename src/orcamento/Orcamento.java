44978839a70b1f5cde51b204439e426d146bf475
João Pedro <jjppgs@gmail.com>
2023-06-13 09:53:50 -0300

    Composite

--- src/orcamento/Orcamento.java
+++ src/orcamento/Orcamento.java
@@ -1,20 +1,22 @@
 package orcamento;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import orcamento.situacao.EmAnalise;
 import orcamento.situacao.Finalizado;
 import orcamento.situacao.SituacaoOrcamento;
 
-public class Orcamento {
+public class Orcamento implements Orcavel{
 
-    private BigDecimal valor;
-    private int quantidadeDeItens;
+    private BigDecimal valor = BigDecimal.ZERO;
     private SituacaoOrcamento situacao;
+    private List<Orcavel> itens;
 
-    public Orcamento(BigDecimal valor, int quantidadeDeItens) {
-        this.valor = valor;
-        this.quantidadeDeItens = quantidadeDeItens;
+    public Orcamento() {
+        this.valor = BigDecimal.ZERO;
+        this.itens = new ArrayList<>();
         this.situacao = new EmAnalise();
     }
 
@@ -36,11 +38,11 @@ public class Orcamento {
     }
 
     public BigDecimal getValor() {
-        return valor;
+        return this.valor;
     }
 
     public int getQuantidadeDeItens() {
-        return quantidadeDeItens;
+        return itens.size();
     }
 
     public SituacaoOrcamento getSituacao() {
@@ -51,8 +53,13 @@ public class Orcamento {
         this.situacao = situacao;
     }
 
-    public boolean isFinalizado(){
-        return situacao instanceof Finalizado;
+    public boolean isFinalizado() {
+        return this.situacao instanceof Finalizado;
+    }
+
+    public void adicionarItem(Orcavel item){
+        this.valor = this.valor.add(item.getValor());
+        this.itens.add(item);
     }
 
 }
